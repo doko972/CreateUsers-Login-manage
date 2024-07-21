fd3dfc19f77136c45af796f6581361f39dca55ac
doko972 david <doko972@gmail.com>
2024-07-21 14:51:43 +0200

    add database new with user and admin profil

--- app/matrice.sql
+++ app/matrice.sql
@@ -1,5 +1,6 @@
 CREATE TABLE img(
    id_img INT AUTO_INCREMENT,
+   image_path VARCHAR(255) NOT NULL,
    PRIMARY KEY(id_img)
 );
 
@@ -9,15 +10,8 @@ CREATE TABLE users(
    email VARCHAR(150) NOT NULL,
    password VARCHAR(50) NOT NULL,
    create_time DATETIME NOT NULL,
+   `role` enum('user','admin') NOT NULL DEFAULT 'user',
    id_img INT NOT NULL,
    PRIMARY KEY(id_users),
    FOREIGN KEY(id_img) REFERENCES img(id_img)
-);
-
-CREATE TABLE manage(
-   id_users_user INT,
-   id_users_admin INT,
-   PRIMARY KEY(id_users_user, id_users_admin),
-   FOREIGN KEY(id_users_user) REFERENCES users(id_users),
-   FOREIGN KEY(id_users_admin) REFERENCES users(id_users)
 );
